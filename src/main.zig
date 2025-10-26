aa5a4aa7598b0da7c0d536a780f0c59ebb4b008e
Vladimir Zarypov <krre31@gmail.com>
2025-10-26 17:26:06 +0200

    Add command line arguments parser

--- src/main.zig
+++ src/main.zig
@@ -3,5 +3,24 @@ const umbra = @import("umbra");
 const print = @import("print.zig");
 
 pub fn main() !void {
-    print.println("Umbra");
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len == 1) {
+        print.println("Umbra");
+        return;
+    }
+
+    const command = args[1];
+
+    if (std.mem.eql(u8, command, "init")) {
+        print.println("init");
+    } else if (std.mem.eql(u8, command, "build")) {
+        print.println("build");
+    } else if (std.mem.eql(u8, command, "run")) {
+        print.println("run");
+    } else {
+        print.println("Unknown command");
+    }
 }
