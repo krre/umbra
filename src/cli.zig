27893de28a235b783dab5ba336075afe518a9448
Vladimir Zarypov <krre31@gmail.com>
2026-07-28 09:06:03 +0300

    Add CLI options

--- src/cli.zig
+++ src/cli.zig
@@ -2,20 +2,44 @@ const std = @import("std");
 const build_options = @import("build_options");
 const print = @import("print.zig");
 const command = @import("command.zig");
+const fatal = std.process.fatal;
+
+const usage =
+    \\Usage: umbra [options] [file]
+    \\
+    \\Options:
+    \\  -h, --help     Print help and exit
+    \\  -v, --version  Print version information and exit
+    \\
+;
 
 pub fn run(io: std.Io, args: []const []const u8) !void {
     if (args.len == 0) {
-        print.printlnf("Umbra {s}", .{build_options.version});
+        print.print(usage);
         return;
     }
 
-    const command_name = args[1];
+    const arg = args[0];
+
+    if (std.mem.startsWith(u8, arg, "-")) {
+        if (std.mem.eql(u8, arg, "-v") or std.mem.eql(u8, arg, "--version")) {
+            print.printlnf("{s}", .{build_options.version});
+            return;
+        } else if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
+            print.print(usage);
+            return;
+        } else {
+            fatal("unrecognized option: '{s}'", .{arg});
+        }
+    }
+
+    const command_name = arg;
 
     if (std.mem.eql(u8, command_name, "init")) {
-        if (args.len < 3) {
+        if (args.len < 2) {
             print.println("Project name is empty");
         } else {
-            try command.init(io, args[2]);
+            try command.init(io, args[1]);
         }
     } else if (std.mem.eql(u8, command_name, "build")) {
         try command.build();
